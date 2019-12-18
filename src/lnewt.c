485dd04f61366e89d731133f02e0d90167b1e11d
tuxd3v <tuxd3v@sapo.pt>
2019-12-18 17:25:47 +0000

    L_WinMessage more cosmetics..

--- src/lnewt.c
+++ src/lnewt.c
@@ -913,7 +913,7 @@ static int L_WinMessage( lua_State *L ) {
 	height = ( with%20 != 0 ) ? ( with + 1 ) : with;
 	
 	lua_pushlstring( L, title, strlen( title ) );
-	lua_pushlstring( L, buttonText, strlen( buttonText );
+	lua_pushlstring( L, buttonText, strlen( buttonText) );
 	lua_pushlstring( L, text, strlen( text ) );
 	
 	/* some action needed.. */
